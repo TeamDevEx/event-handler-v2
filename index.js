f91281aa9f08a52f6c0b179feb0039d238162393
Lendly Cagata <lendly.cagata@telus.com>
2023-12-10 01:47:18 +0800

    chore: 1

--- index.js
+++ index.js
@@ -6,37 +6,74 @@ const storage = new Storage({
   projectId: "off-net-dev",
   credentials: require("C:/Users/lendly.cagata.LTPHDISTIDLCAGA/Desktop/Dora/dora-sa"),
 });
+const currentDate = moment().format("YYYY-MM-DD_HH-mm-ss");
+const bucketName = "github-flow";
+const folderName = "webhook-event/test";
+const baseFile = `webhook-event/test/event_1_${currentDate}`;
 
-async function checkBatchFile() {
-  const storage = new Storage({
-    projectId: "off-net-dev",
-    credentials: require("C:/Users/lendly.cagata.LTPHDISTIDLCAGA/Desktop/Dora/dora-sa"),
+getTargetFile = async (baseFiles) => {
+  let maxNumber = -Infinity;
+  let targetFile = null;
+
+  baseFiles.forEach((str) => {
+    const [_, __, segment] = str.split("_");
+
+    if (segment) {
+      const numbers = segment.match(/\d+/g);
+
+      if (numbers) {
+        const maxInString = Math.max(...numbers.map(Number));
+
+        if (maxInString > maxNumber) {
+          maxNumber = maxInString;
+          targetFile = str;
+        }
+      }
+    }
   });
 
+  return targetFile;
+};
+
+checkBatchFile = async () => {
   try {
-    const bucket = storage.bucket(`github-flow`);
-    let baseFile = bucket.file(`webhook-event/github/test.json`);
-    const [baseFileExists] = await baseFile.exists();
-    if (!baseFileExists) {
+    const [files] = await storage.bucket(bucketName).getFiles({
+      prefix: folderName,
+    });
+    const baseFiles = files.map((file) => file.metadata.name);
+    const targetExists = await getTargetFile(baseFiles);
+    console.log("Does target exist? ", targetExists);
+
+    const targetExists_download = storage.bucket(bucketName).file(targetExists);
+
+    const [record] = await targetExists_download.download();
+    const recordData = JSON.parse(record.toString());
+    console.log("Count exist object", recordData.length);
+
+    if (
+      !targetExists ||
+      targetExists === null ||
+      (targetExists === undefined && recordData.length >= 200)
+    ) {
+      console.log("CREATE A NEW BASE FILE");
       await baseFile.save(JSON.stringify([]));
       console.log("CREATE A NEW BASE FILE");
-      return "good";
+      return { file: baseFile, status: "success" };
     } else {
       console.log("BASE FILE STILL EXIST");
-      return "good";
+      return { file: targetExists, status: "success" };
     }
   } catch (error) {
     return error;
   }
-}
+};
 
-async function uploadToGCS(payload) {
+uploadToGCS = async (payload) => {
   const checkBucket = await checkBatchFile();
-  if (checkBucket == "good") {
+  console.log("checkBucket status ", checkBucket.status);
+  if (checkBucket.status == "success") {
     try {
-      const getBaseFile = storage
-        .bucket("github-flow")
-        .file(`webhook-event/github/test.json`);
+      const getBaseFile = storage.bucket(bucketName).file(checkBucket.file);
 
       const [file] = await getBaseFile.download();
       const existingData = JSON.parse(file.toString());
@@ -45,13 +82,15 @@ async function uploadToGCS(payload) {
       console.log("Count additional object", existingData.length);
 
       await storage
-        .bucket("github-flow")
-        .file("webhook-event/github/test.json")
+        .bucket(bucketName)
+        .file(checkBucket.file)
         .save(JSON.stringify(existingData));
     } catch (error) {
       console.error("Error uploading JSON data to GCS:", error);
     }
   }
-}
+};
+
+exports.default = { checkBatchFile, getTargetFile };
 
 uploadToGCS(fs.readFileSync("deployment_status.json", "utf-8"));
